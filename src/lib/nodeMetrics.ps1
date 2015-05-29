a755f8c7374843d94d169a8fe5d67df0671483c0
Ben Newton <istairbn@users.noreply.github.com>
2015-05-29 09:12:36 +0100

    Update nodeMetrics.ps1
    Complete redesign

--- src/lib/nodeMetrics.ps1
+++ src/lib/nodeMetrics.ps1
@@ -1,1 +1,22 @@
-﻿#This calculates the system calls of all machines$wait = 30Add-PSSnapIn Microsoft.HPC;while(1) {$timestamp = Get-Date -Format "yyyy/MM/dd HH:mm:ss zzz"$OUT = Get-HpcMetricValue -name HPCContextSwitches,HPCCoresInUse,HPCDiskQueue,HPCDiskThroughput,HPCJobsRunning,HPCMemoryPaging,HPCSystemCalls,HPCTasksRunning,HPCWcfBrokerCalls,HPCWcfFailedBrokerCalls,HPCWcfIncomingCalls,HPCWcfRetrievedResponse   foreach ($RECORD in $OUT)   {      $LOG = $timestamp  + "," +  $RECORD.NodeName  + "," +  $RECORD.Metric  + "," +  $RECORD.Value  + "," + $Record.Counter        Write-Host $LOG     }	Sleep $wait}
+#This calculates the system calls of all machines
+$wait = 30
+
+Add-PSSnapIn Microsoft.HPC;
+
+while(1) {
+$timestamp = Get-Date -Format "yyyy/MM/dd HH:mm:ss zzz"
+
+$COLLECT = Get-HpcNode
+ foreach ($NODE in $COLLECT) 
+ {
+  $ARRAY = @()
+    $OUT = Get-HpcMetricValue -Node $NODE
+    foreach ($LINE in $OUT){
+    $ARRAY += ($LINE.Metric.ToString() + "=" + $LINE.Value.ToString()) 
+    }
+    $LOG = Write-Host $timestamp "{ `"Node`":`""$NODE.NetBiosName"`",`"NodeState`":`""$NODE.NodeState"`",`"NodeHealth`":`""$NODE.NodeHealth"`",`"ServiceHealth`":`""$Node.ServiceHealth"`",`"Template`":`""$NODE.Template"`",`"Groups`":`""$NODE.Groups"`",`"Location`":`""$NODE.Location"`",`"Cores`":`""$NODE.ProcessorCores"`",`"SubscribedCores`":`""$NODE.SubscribedCores"`",`"HealthState`":`""$NODE.HealthState"`",`"Affinity`":`""$Node.Affinity"`",`"Sockets`":`""$Node.Sockets"`",`"SubscribedSockets`":`""$NODE.SubscribedSockets"`",`"Role`":`""$NODE.NodeRole "`",`"Version`":`""$NODE.Version "`",`"NodeStats`":`""$ARRAY"`" }"
+    $LOG
+    
+}
+Sleep $wait
+}
